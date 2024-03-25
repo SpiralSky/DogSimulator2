8aca8046f8de393d8e9aefdc3dddbb2efb667b1f
Tung Shi An <shianastrophysics@gmail.com>
2024-03-25 00:24:27 +0000

    Added files

---
+++ dogsimulator2/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/DogSimulator2",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+};
+
+export default nextConfig;
