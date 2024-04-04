cfa3860f1ba7c54ba3153c1e7f0441d87786e74e
Tung Shi An <shianastrophysics@gmail.com>
2024-04-04 03:26:26 +0000

    changed app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const app = new PIXI.Application({ width: 800, height: 600 });
+const app = new PIXI.Application({ width: 800, height: 600 , backgroundColor: 0x2980b});
 document.body.appendChild(app.view);
 
 const sprite = PIXI.Sprite.from('path/to/your/image.png');
