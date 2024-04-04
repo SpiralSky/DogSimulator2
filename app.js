773606add0ccf3a2c9fe3cea0637d7023c3e306c
Tung Shi An <shianastrophysics@gmail.com>
2024-04-04 04:44:38 +0000

    Switched file to app.js

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 import { Application } from 'pixi.js';
 
-// Asynchronous IIFE
-(async () =>
-{
-    // Create a PixiJS application.
-    const app = new Application();
+// Create the application helper and add its render target to the page
+const app = new PIXI.Application();
+await app.init({ width: 640, height: 360 })
+document.body.appendChild(app.canvas);
 
-    // Intialize the application.
-    await app.init({ background: '#1099bb', resizeTo: window });
+// Create the sprite and add it to the stage
+await PIXI.Assets.load('sample.png');
+let sprite = PIXI.Sprite.from('sample.png');
+app.stage.addChild(sprite);
 
-    // Then adding the application's canvas to the DOM body.
-    document.body.appendChild(app.canvas);
-})();
+// Add a ticker callback to move the sprite back and forth
+let elapsed = 0.0;
+app.ticker.add((ticker) => {
+elapsed += ticker.deltaTime;
+sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+});
