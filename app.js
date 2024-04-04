948eac7edd583bb5f73babb55e2e05461088a3d3
Tung Shi An <shianastrophysics@gmail.com>
2024-04-04 03:31:40 +0000

    updated app.js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-const app = new PIXI.Application({ width: 800, height: 600 , backgroundColor: 0x2980b});
-document.body.appendChild(app.view);
+import { Application } from 'pixi.js';
 
-const sprite = PIXI.Sprite.from('path/to/your/image.png');
-app.stage.addChild(sprite);
+// Asynchronous IIFE
+(async () =>
+{
+    // Create a PixiJS application.
+    const app = new Application();
+
+    // Intialize the application.
+    await app.init({ background: '#1099bb', resizeTo: window });
+
+    // Then adding the application's canvas to the DOM body.
+    document.body.appendChild(app.canvas);
+})();
